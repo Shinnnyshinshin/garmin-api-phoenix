52aef5b9c5c95e303719e7198bf9bcd0e572f622
William Shin <willshin@gmail.com>
2026-02-26 17:09:12 -0800

    dev.exs is changed too

--- config/dev.exs
+++ config/dev.exs
@@ -55,12 +55,12 @@ config :garmin_planner, GarminPlannerWeb.Endpoint,
     web_console_logger: true,
     patterns: [
       # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
+      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$",
       # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
+      ~r"priv/gettext/.*\.po$",
       # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/garmin_planner_web/router\.ex$"E,
-      ~r"lib/garmin_planner_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"lib/garmin_planner_web/router\.ex$",
+      ~r"lib/garmin_planner_web/(controllers|live|components)/.*\.(ex|heex)$"
     ]
   ]
 
